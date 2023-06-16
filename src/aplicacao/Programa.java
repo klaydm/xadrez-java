38f6d3dcf7d5645a9ac22c3d0eb2b3e1be8eaa52
Klaydm <klaydm@gmail.comqexirgit config --global user.email klaydm@gmail.com>
2023-06-16 09:33:08 -0300

    Pecas do xadrez, cores e impressao do tabuleiro

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -2,11 +2,13 @@ package aplicacao;
 
 import tabuleiro.Posicao;
 import tabuleiro.Tabuleiro;
+import xadrez.PartidaDeXadrez;
 
 public class Programa {
     public static void main(String[] args) {
 
-        Tabuleiro tabuleiro = new Tabuleiro(8, 8);
+        PartidaDeXadrez partida = new PartidaDeXadrez();
+        UI.imprimirTabuleiro(partida.getPecas());
 
     }
 }
